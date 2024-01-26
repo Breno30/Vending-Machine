a017c74c813681b6dead17bbb05ef96db2c61719
breno30 <brenodonascimentosilvaa@gmail.com>
2024-01-26 20:30:05 -0300

    create machine factory

---
+++ database/factories/MachineFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Machines>
+ */
+class MachineFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'latitude' => fake()->latitude(),
+            'longitude' => fake()->longitude(),
+            'owner_id' => User::inRandomOrder()->first(),
+        ];
+    }
+}
