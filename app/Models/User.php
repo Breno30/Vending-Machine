4ebe3feb5fce04e7970b57c0740b77e608d2bd59
breno30 <brenodonascimentosilvaa@gmail.com>
2024-01-26 20:28:45 -0300

    add method to get machines' user

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function machines() {
+        return $this->hasMany(Machine::class);
+    }
 }
