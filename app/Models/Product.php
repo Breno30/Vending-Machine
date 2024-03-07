d23d4fe2b70f73f5e44ad964ccf58f41bdb55167
breno30 <brenodonascimentosilvaa@gmail.com>
2024-03-07 20:47:58 -0300

    delete image when deleting product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'price', 'description', 'image'];
+
+    protected static function boot() {
+        parent::boot();
+
+        static::deleting(function ($product) {
+            Storage::delete($product->image);
+        });
+    }
 
     public function machines() {
         return $this->belongsToMany(Machine::class)->withPivot('quantity');
