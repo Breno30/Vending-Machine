87653e9c620762548cff95f56d21e2dc20758555
breno30 <brenodonascimentosilvaa@gmail.com>
2024-04-24 07:18:21 -0300

    Update email template to render HTML and use variables for dynamic content

--- resources/views/emails/email-transaction-paid.blade.php
+++ resources/views/emails/email-transaction-paid.blade.php
@@ -1,5 +1,11 @@
 <x-mail::message>
-<p>Purchase of the product <b>"{{$event->product->name}}"</b> paid by <b>{{$event->transaction->type}}</b> with the value of <b>{{env('CURRENCY')}} {{$event->product->price}}</b></p>
+<p>{!! __('Purchase of the product <b>":productName"</b> paid by <b>:transactionType</b> with the value of <b>:currency :price</b>',
+        [
+            'productName' => $event->product->name,
+            'transactionType' => $event->transaction->type,
+            'currency' => env('CURRENCY'),
+            'price' => $event->product->price
+        ]) !!}</p>
 <iframe src="https://maps.google.com/maps?q=10.305385,77.923029&amp;hl=es;z=14&amp;output=embed" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">></iframe>
-<x-mail::button :url="$event->transaction->id">Click here to see the transaction</x-mail::button>
+<x-mail::button :url="$event->transaction->id">{{ __('Click here to see the transaction') }}</x-mail::button>
 </x-mail::message>
