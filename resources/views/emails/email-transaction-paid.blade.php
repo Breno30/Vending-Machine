48437c08362fe023f0041f1e481beb543f435310
breno30 <brenodonascimentosilvaa@gmail.com>
2024-04-24 19:56:45 -0300

    get correct coordinates

--- resources/views/emails/email-transaction-paid.blade.php
+++ resources/views/emails/email-transaction-paid.blade.php
@@ -6,6 +6,6 @@
             'currency' => env('CURRENCY'),
             'price' => $event->product->price
         ]) !!}</p>
-<iframe src="https://maps.google.com/maps?q=10.305385,77.923029&amp;hl=es;z=14&amp;output=embed" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">></iframe>
+<iframe src="https://maps.google.com/maps?q={{ $event->machine->latitude }},{{ $event->machine->longitude }}&amp;hl=es;z=14&amp;output=embed" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">></iframe>
 <x-mail::button :url="$event->transaction->id">{{ __('Click here to see the transaction') }}</x-mail::button>
 </x-mail::message>
