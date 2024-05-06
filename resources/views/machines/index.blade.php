7f98b9cb816f128d58bd3bc761c1eceeba756248
breno30 <brenodonascimentosilvaa@gmail.com>
2024-05-06 20:18:52 -0300

    isolate div into blade

--- resources/views/machines/index.blade.php
+++ resources/views/machines/index.blade.php
@@ -4,21 +4,14 @@
             {{ __('Machines') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    @foreach ($machines as $machine)
-                    <div class="bg-white dark:bg-gray-800 hover:opacity-80 overflow-hidden shadow-sm sm:rounded-lg p-4  place-self-center w-fit">
-                        <a href="{{ route('machines.show', ['machine' => $machine->id]) }}" class="grid gap-2">
-                            <img src="{{ asset('images/icon-vending-machine.png') }}" alt="{{ $machine->label }}">
-                            <h4 class="text-md font-semibold text-center">{{ $machine->label }}</h4>
-                        </a>
-                    </div>
-                    @endforeach
-                </div>
-            </div>
+    <x-middle>
+        @foreach ($machines as $machine)
+        <div class="bg-white dark:bg-gray-800 hover:opacity-80 overflow-hidden shadow-sm sm:rounded-lg p-4  place-self-center w-fit">
+            <a href="{{ route('machines.show', ['machine' => $machine->id]) }}" class="grid gap-2">
+                <img src="{{ asset('images/icon-vending-machine.png') }}" alt="{{ $machine->label }}">
+                <h4 class="text-md font-semibold text-center">{{ $machine->label }}</h4>
+            </a>
         </div>
-    </div>
+        @endforeach
+    </x-middle>
 </x-app-layout>
